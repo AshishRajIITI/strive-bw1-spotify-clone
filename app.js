47bdabc1eef6d72c239cd322a71f94cf7e512824
Sean Knowles <s@seanknowles.dev>
2021-03-03 11:16:13 +0000

    added font awesome library to index page, added icon to menu list, some minor margin improvements to side-navbar

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 console.log('app.js connected')
-
 import { navbar } from './components/navbar.js'
+
+const state = {
+  user: {
+    uui: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: ''
+  }
+}
 
 window.onload = function () {
   navbar('navbar-injector')
